e5151fdcc4364c20354aa14305298dfb4588f107
Jonna Laine <jonna.jp.laine@gmail.com>
2024-08-19 20:00:54 +0000

    add user authentication

--- nature_adventures/urls.py
+++ nature_adventures/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path("", include("profiles.urls"), name="profile-urls"),
     path("", include("hikes.urls"), name="hikes-urls"),
-    path('admin/', admin.site.urls),
+    
 
 ]
