28fecb9ad9622847790e27294b079f2fa990510f
Jonna Laine <jonna.jp.laine@gmail.com>
2024-09-08 20:39:00 +0000

    feat: add link to liked hikes to profile

--- profiles/views.py
+++ profiles/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.template.defaultfilters import slugify
 from .models import Profile
 from .forms import UpdateProfileForm
+from hikes.models import Like
 
 # Create your views here.
 
@@ -13,15 +15,18 @@ def profile(request, username):
     users = User.objects.all()
     username = get_object_or_404(users, username=username)
     # Get the profile of the user using the username
-    queryset = Profile.objects.all()
-    profile = get_object_or_404(queryset, user=username)
+    profiles = Profile.objects.all()
+    profile = get_object_or_404(profiles, user=username)
     # Get the hikes that the user has added
     user_hikes = username.hiking_routes.all()
+    # Get the hikes the user has liked
+    user_likes = list(Like.objects.filter(user=username).values_list("hike__hike_name", flat=True))
     return render(
             request, 
             "profiles/profile.html", 
             {"profile":profile,
-            "user_hikes": user_hikes,},
+            "user_hikes": user_hikes,
+            "user_likes": user_likes,},
             )
 
 
