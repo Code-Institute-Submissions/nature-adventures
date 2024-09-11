c205d4055da453ff7292c58f77e7d0b4abe059ad
Jonna Laine <jonna.jp.laine@gmail.com>
2024-09-11 20:06:31 +0000

    feat: create 404 page

--- nature_adventures/urls.py
+++ nature_adventures/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("profiles/", include("profiles.urls"), name="profile-urls"),
     path("", include("hikes.urls"), name="hikes-urls"),
+    path(
+        '404',
+        TemplateView.as_view(template_name="404.html"),
+        name='404'
+    ),
 ]
